91923f06a87800a1e4a6960f70dc80050634854c
adamsbruhmoment <144424983+adamsbruhmoment@users.noreply.github.com>
2024-04-16 09:13:05 +0200

    Update edge.py
    Gaussian Blur

--- edge.py
+++ edge.py
@@ -1,4 +1,12 @@
 import cv2
 
+
+img_test = cv2.imread('King Domino dataset\\Cropped and perspective corrected boards\\6.jpg')
+#Blur image. Det gør allegedly edge detection bedre
+img_blur = cv2.GaussianBlur(img_test,(3,3), SigmaX=0, SigmaY=0)
+
+cv2.imshow("Original", img_blur)
+cv2.waitKey(0)
+
 def edgeDetection(image):
     pass
