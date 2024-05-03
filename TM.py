46e74eb500df744c52887dd39ae144dc1cf37089
Rubenk44 <rubenkronborg@gmail.com>
2024-05-03 10:59:55 +0200

    22

--- TM.py
+++ TM.py
@@ -1,27 +1,28 @@
 import cv2 as cv
 import numpy as np
+import os
 
-def Template_matching(Template, image, threshold):
-    threshold = threshold
-    # Reading Template and the board image
-    template = cv.imread(Template, 0)
-    # Saving the width and height
-    w, h = template.shape[::-1]
-    image_rgb = cv.imread(image)
-    # Converting to grayscale
-    image_gray = cv.cvtColor(image_rgb, cv.COLOR_BGR2GRAY)
-    res = cv.matchTemplate(image_gray, template, cv.TM_CCOEFF_NORMED)
-    loc = np.where(res >= threshold)
-    print("Found", len(loc[0]), "matches")
-    for pt in zip(*loc[::-1]):
-        cv.rectangle(image_rgb, pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
+def Template_matching(Template_folder, image, threshold):
+    img_rgb = cv.imread(image)
+    img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
 
-    # Show the final image with the matched area.
-    cv.imshow('Detected', image_rgb)
-    return image_rgb
-    
+    for file_name in Template_folder:
+        template_path = os.path.join(r"/Users/rubenkronborg/Desktop/Templates", file_name)
+        template = cv.imread(template_path, cv.IMREAD_GRAYSCALE)
+        w, h = template.shape[::-1]
+
+        res = cv.matchTemplate(img_gray, template, cv.TM_CCOEFF_NORMED)
+        loc = np.where(res >= threshold)
+
+        for pt in zip(*loc[::-1]):
+            cv.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+
+    return img_rgb
+
+
 def main():
-    result_image = Template_matching("crownblur.png", r"/Users/rubenkronborg/Desktop/Universitet/2. Semester/DUAS/MP-p2-DUAS/King Domino dataset/Cropped and perspective corrected boards/1.jpg",0.5)
+    template_folder = os.listdir(r"/Users/rubenkronborg/Desktop/Templates") 
+    result_image = Template_matching(template_folder, r"King Domino dataset/Cropped and perspective corrected boards/25.jpg", 0.7)
     cv.imshow('Detected', result_image)
     cv.waitKey(0)
     cv.destroyAllWindows()
